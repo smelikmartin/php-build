c93ff07430ee8fa23e44adadd6be0f40b9145f29
Martin Smelik <smelikmartin@gmail.com>
2019-08-26 15:00:11 +0200

    Fix ubuntu version

--- image/packages.sh
+++ image/packages.sh
@@ -15,7 +15,7 @@ echo 'deb https://deb.nodesource.com/node_10.x bionic main' > /etc/apt/sources.l
 
 ## MSSQL repository
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
 ACCEPT_EULA=Y $minimal_apt_get_install msodbcsql17
 ACCEPT_EULA=Y $minimal_apt_get_install unixodbc-dev mssql-tools
