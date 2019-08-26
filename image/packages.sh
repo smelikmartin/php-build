064576b3d5a95d5c121d9a9b7f6b0f22a4cd6a4c
Martin Smelik <smelikmartin@gmail.com>
2019-08-26 14:53:29 +0200

    Added support for sqlsrv driver and mssql

--- image/packages.sh
+++ image/packages.sh
@@ -13,7 +13,12 @@ LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 echo 'deb https://deb.nodesource.com/node_10.x bionic main' > /etc/apt/sources.list.d/nodesource.list
 
+## MSSQL repository
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
+ACCEPT_EULA=Y $minimal_apt_get_install msodbcsql17
+ACCEPT_EULA=Y $minimal_apt_get_install unixodbc-dev mssql-tools
 
 ## NodeJS
 $minimal_apt_get_install nodejs
@@ -24,6 +29,7 @@ $minimal_apt_get_install \
 	php$1-bz2 \
 	php$1-cli \
 	php$1-curl \
+	php$1-dev \
 	php$1-gd \
 	php$1-gmp \
 	php$1-imap \
@@ -39,7 +45,8 @@ $minimal_apt_get_install \
 	php$1-xml \
 	php$1-zip \
 	php-mongodb \
-	php-redis
+	php-redis \
+	php-pear
 
 PHP_VER=`echo $1 | sed -e 's/\.//g'`
 if [ "$PHP_VER" -lt "72" ]; then
@@ -48,6 +55,15 @@ fi
 if [ "$PHP_VER" -gt "70" ]; then
 	$minimal_apt_get_install php-sodium
 fi
+
+# SQLSRV drivers
+$minimal_apt_get_install -y g++ make
+pecl install sqlsrv
+pecl install pdo_sqlsrv
+echo "extension=sqlsrv.so" > /etc/php/$1/mods-available/sqlsrv.ini
+echo "extension=pdo_sqlsrv.so" > /etc/php/$1/mods-available/pdo_sqlsrv.ini
+ln -s /etc/php/$1/mods-available/sqlsrv.ini /etc/php/$1/cli/conf.d/20-sqlsrv.ini
+ln -s /etc/php/$1/mods-available/pdo_sqlsrv.ini /etc/php/$1/cli/conf.d/20-pdo_sqlsrv.ini
 
 # Grunt and Gulp
 npm install -g grunt gulp
